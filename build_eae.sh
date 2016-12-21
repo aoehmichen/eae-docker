76a24ad57a14beed77e1dbad326f5776f0a8d89a
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-12-21 12:31:54 +0000

    this looks tons better

--- build_eae.sh
+++ build_eae.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# We generate the same key for all containers
+## Cleanup before build
+rm -f id_rsa*
+rm -f interfaceEAE/id_rsa*
+rm -f jupyter/id_rsa.pub
+
+## We generate the same key for all containers
 ssh-keygen -t rsa -N "" -f id_rsa
 
+## We copy to the directories
 cp  id_rsa interfaceEAE
 cp  id_rsa.pub interfaceEAE
-
 cp id_rsa.pub jupyter
 
-# We build the images
+## We build the images
 Docker build interfaceEAE/
 Docker build jupyter/
