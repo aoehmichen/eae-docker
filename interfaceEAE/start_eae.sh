5c34ff8b791819e8d7d73b797500e5b4cdafa748
axelfrancois.oehmichen11@imperial.ac.uk <axelfranc.oehmichen11@imperial.ac.uk>
2016-12-21 17:28:39 +0000

    ssssss

--- interfaceEAE/start_eae.sh
+++ interfaceEAE/start_eae.sh
@@ -6,14 +6,6 @@
 # We restart the openlava services
 service openlava restart
 
-# We set up the ssl certificate and https for tomcat
-/usr/lib/jvm/default-java/bin/keytool -genkey -alias tomcat -keyalg RSA -storepass changeit -keypass changeit -validity 365 -dname "cn=AOehmichen, ou=eAE, o=DSI, c=UK"
-sed -i -e '89d;93d' /var/lib/tomcat7/conf/server.xml
-sed -i "s/sslProtocol=\"TLS\"/sslProtocol=\"TLS\"  keystorePass=\"changeit\"/g" /var/lib/tomcat7/conf/server.xml
-
-echo "Host *
-    StrictHostKeyChecking no" > /home/eae/.ssh/config
-
 # We restart tomcat
 killall java
 service tomcat7 restart
