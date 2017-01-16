be2bdba4f89ee71f15aacf8b9257fce5ffd6a0d2
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-01-16 01:54:53 +0000

    removing useless docker compose and forcing restart of openlava

--- workerExample/start_eae.sh
+++ workerExample/start_eae.sh
@@ -3,14 +3,17 @@
 # We get the environment set to get access to the openlava's bhosts, bqueues etc...
 . /etc/profile.d/openlava.sh
 
-# We restart the openlava services
-service openlava restart
+# We restart the ssh services
 service ssh restart
 
 cat /home/eae/.ssh/id_rsa.pub > /home/eae/.ssh/authorized_keys
 chown -R eae:eae /home/eae/.ssh/
 chmod 600 /home/eae/.ssh/id_rsa
 chown openlava:openlava /opt/openlava-3.3/etc/*
+
+# We restart the openlava service after we wait for the interface node to startup
+sleep 20
+service openlava restart
 
 ## Hook on the container
 if [[ $1 == "-deamon" ]]; then
