8ebb9389289357346fc9ce8be992a26b2cb44f2e
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-01-30 23:45:34 +0000

    final working version of the docker compose

--- interfaceEAE/Config.groovy
+++ interfaceEAE/Config.groovy
@@ -4,5 +4,5 @@ com.eae.mongoUserdatabse = 'eae'
 com.eae.mongoPassword = 'eae'
 
 com.eae.openLavaEnv = '/etc/profile.d/openlava.sh'
-com.eae.analyticsScriptsDir = '/root/AnalyticsEAE/'
-com.eae.localDataStore = '/root/data/'
+com.eae.analyticsScriptsDir = '/home/eae/AnalyticsEAE/'
+com.eae.localDataStore = '/home/eae/data/'
