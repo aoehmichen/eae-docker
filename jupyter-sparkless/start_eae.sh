8b118d237eeed3914ca2a496c6a87b26b7bd6d86
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-01-31 00:13:17 +0000

    creating a ightweight docker container for DL and a spark worker example.
    TO BE TESTED

---
+++ jupyter-sparkless/start_eae.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+args=("$@")
+START_MODE=${args[0]}
+ADDRESS=${args[1]}
+PORT=${args[2]}
+HOST_IP=${args[3]}
+SSH_HOST_PORT=${args[4]}
+PASSWORD="${args[5]}"
+
+## Safe guard against empty parameters
+if [[ -z $ADDRESS ]] || [[ -z $PORT ]] || [[ -z $HOST_IP ]] || [[ -z $SSH_HOST_PORT ]]; then
+    echo "Interface eAE ADDRESS or PORT or HOST_IP or SSH_HOST_PORT is not set!"
+    echo "Interface eAE ADDRESS: $ADDRESS"
+    echo "Interface eAE PORT: $PORT"
+    echo "IP of the host machine: $HOST_IP"
+    echo "Host machine port where ssh is mapped: $SSH_HOST_PORT"
+    exit 1
+fi
+echo "Interface eAE ADDRESS: $ADDRESS"
+echo "Interface eAE PORT: $PORT"
+echo "Notebook password Hash: $PASSWORD"
+echo "IP of the host machine: $HOST_IP"
+echo "Host machine port where ssh is mapped: $SSH_HOST_PORT"
+
+## We restart all the ssh service
+service ssh restart
+
+
+## We copy the files to their rightful place so we can edit them on the flight later.
+yes | cp -rf /root/id_rsa /home/eae/.ssh/id_rsa
+yes | cp -rf /root/id_rsa.pub /home/eae/.ssh/id_rsa.pub
+yes | cp -rf /root/jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
+cat /home/eae/.ssh/id_rsa.pub > /home/eae/.ssh/authorized_keys
+chmod 600 /home/eae/.ssh/id_rsa
+chown -R eae:eae /home/eae/.ssh/
+
+## We configure the ssl certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -subj "/C=UK/ST=Denial/L=London/O=DSI/CN=$ADDRESS" -keyout notebook.key -out notebook.pem
+
+## We set the eae configurations (IP + port) and password for Jupyter
+## You can prepare a hashed password using the function notebook.auth.security.passwd('password')
+if [[ ! -z $PASSWORD ]]; then
+    sed -i "s/sha1:854cd6632c4a:532c127d43cb1ff05745850979f4c863e3351da8/$PASSWORD/g" /root/.jupyter/jupyter_notebook_config.py
+fi
+
+# We edit the default configurations
+sed -i "s/.eae_ip = 'interfaceeae'/.eae_ip = '$ADDRESS'/g" /root/.jupyter/jupyter_notebook_config.py
+sed -i "s/.eae_port = 8443/.eae_port = $PORT/g" /root/.jupyter/jupyter_notebook_config.py
+sed -i "s/.eae_host_ip = 'jupytereae'/.eae_host_ip = '$HOST_IP'/g" /root/.jupyter/jupyter_notebook_config.py
+sed -i "s/.eae_host_ssh_port = 22/.eae_host_ssh_port = $SSH_HOST_PORT/g" /root/.jupyter/jupyter_notebook_config.py
+
+## We start the notebook server in the jupyter folder
+cd /home/eae/jupyter
+#screen -mdS jupyter jupyter-notebook --allow-root --kernel=ir
+
+## Hook on the container
+if [[ $START_MODE == "-deamon" ]]; then
+  jupyter-notebook --allow-root --kernel=ir
+fi
+
+if [[ $START_MODE == "-bash" ]]; then
+  /bin/bash
+fi
---
+++ workerSparkExample/start_eae.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# We get the environment set to get access to the openlava's bhosts, bqueues etc...
+. /etc/profile.d/openlava.sh
+
+# We restart the ssh services
+service ssh restart
+
+## Hosts file configuration change for CDH to work properly
+sed "/eae-jupyter/d" /etc/hosts |  sed '/127.0.0.1/c  127.0.0.1 eae-jupyter' > /tmp/sed.txt
+cat /tmp/sed.txt > /etc/hosts
+
+## We restart all the necessary Cloudera services
+service cloudera-scm-server-db restart
+sleep 60
+service cloudera-scm-agent restart
+sleep 120
+service cloudera-scm-server restart
+sleep 60
+
+## We restart all the Hadoop services
+python /root/Cloudera_Management.py
+
+cat /home/eae/.ssh/id_rsa.pub > /home/eae/.ssh/authorized_keys
+chown -R eae:eae /home/eae/.ssh/
+chmod 600 /home/eae/.ssh/id_rsa
+chown openlava:openlava /opt/openlava-3.3/etc/*
+
+# We restart the openlava service after we wait for the interface node to startup
+sleep 20
+service openlava restart
+
+## Hook on the container
+if [[ $1 == "-deamon" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
