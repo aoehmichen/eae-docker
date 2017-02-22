b270d419e51f9707255cb1b4b5f7f62f492bd24c
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-02-22 16:27:28 +0000

    let's try this

--- build_eae.sh
+++ build_eae.sh
@@ -15,50 +15,13 @@ fi
 
 rm -rf ssh/*
 mv id_rsa* ssh/
-cp interfaceEAE/lsf.cluster.* workerExample
+
 cp jupyter/irkernel_install.r workerExample
+cp interfaceEAE/lsb.* workerExample
+cp interfaceEAE/lsb.* workerSparkExample
+cp interfaceEAE/lsf.cluster.* workerExample
 cp interfaceEAE/lsf.cluster.* workerSparkExample
 
-docker-compose build
-
-echo -e "To start the eAE please run 'bash start_eae.sh'\nAlternatively, if you do not want to run tranSMART at the same time, please run 'bash start_eae_without_transmart.sh'\n"
-
-
-## We build the images
-#docker build interfaceEAE/
-#docker build jupyter/
-#docker build workerExample/
-#
-## NB -it to be replaced by -d
-### InterfaceEAE start
-#docker run -it \
-#            -h interfaceEAE \
-#            --link mongo_mongo_1 \
-#            --net mongo_default \
-#            -v ~/.id_rsa.pub:/root/.ssh/.id_rsa.pub
-#            -p 22222:22 \
-#            -p 8443:8443 \
-#            -p 16322:16322 \
-#            -p 16323:16323 \
-#            -p 16324:16324 \
-#            -p 16325:16325 \
-#            --add-host worker1:$WorkerIP \
-#            aoehmichen/interfaceEAE:latest -bash None
-#
-#docker run -it \
-#            -h worker1 \
-#            -p 22222:22 \
-#            -p 16322:16322 \
-#            -p 16323:16323 \
-#            -p 16324:16324 \
-#            -p 16325:16325 \
-#            --add-host interfaceEAE:146.169.33.20 \
-#            xxxxxxxx -bash
-#
-#docker run -it \
-#            -h interfaceEAE \
-#            -v ./id_rsa.pub:/home/eae/.ssh/id_rsa.pub \
-#            -v ./interfaceEAE/Config.groovy:/home/eae/.grails/transmartConfig/Config.groovy \
-#            -v ./id_rsa:/home/eae/.ssh/id_rsa \
-#            -v ./interfaceEAE/lsb.hosts :/opt/openlava-3.3/etc/lsb.hosts \
-#            aoehmichen/interfaceEAE:latest
+docker build interfaceEAE
+docker build workerExample
+docker build workerSparkExample
